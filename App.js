bf4a6c590d56bcf812224987e4f103439051fe48
Yury <pozhidaev.yury98@gmail.com>
2021-09-28 02:21:00 +0700

    Init commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>Hello World</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  nameText: {
+    fontSize: 50,
+    padding: 15,
+  }
+});
